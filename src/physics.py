530b979ce3393581b3c3aed09b74cc3b3c9200b3
Puce807 <luke.clarkeson@gmail.com>
2026-06-29 18:23:27 +0100

    1 - Added error handling to physics calculations. Added initial folder structure `src/physics.py` and `src/scraper.py`

---
+++ src/physics.py
@@ -0,0 +1,45 @@
+import math
+
+def find_distance(parallax_mas: float) -> float | None:
+    """Calculates distance in Parsecs using parallax angle in milliarcseconds"""
+    if parallax_mas is None:
+        return None
+    if parallax_mas <= 0:
+        return None
+
+    try:
+        return 1000 / parallax_mas
+    except ZeroDivisionError:
+        return None
+
+def find_colour_index(bp: float, rp: float) -> float | None:
+    """Calculates colour index (surface temperature of a star)"""
+    if bp is None or rp is None:
+        return None
+
+    return bp - rp
+
+def find_absolute_magnitude(apparent_magnitude: float, distance_parsecs: float) -> float | None:
+    """Calculate absolute magnitude using the distance modulus formula."""
+    if apparent_magnitude is None:
+        return None
+    if distance_parsecs is None or distance_parsecs <= 0:
+        return None
+
+    return apparent_magnitude - 5 * math.log10(distance_parsecs) + 5
+
+def estimate_temperature(colour_index: float) -> float | None:
+    """Estimate stellar effective temperature from BP-RP colour index."""
+    if colour_index is None:
+        return None
+    try:
+        return 4600 * (1 / (0.92 * colour_index + 1.7) + 1 / (0.92 * colour_index + 0.62))
+    except ZeroDivisionError:
+        return None
+
+def find_luminosity(absolute_magnitude: float) -> float | None:
+    """Calculate luminosity relative to the sun (L☉) from absolute magnitude"""
+    sun_absolute_magnitude = 4.83
+    if absolute_magnitude is None:
+        return None
+    return 10 ** ((sun_absolute_magnitude - absolute_magnitude) / 2.5)
---
+++ src/scraper.py
@@ -0,0 +1,26 @@
+from astroquery.gaia import Gaia
+
+Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source" # TODO: ADD TO CONFIG FILE
+
+adql_query = """
+SELECT TOP 1
+    source_id, ra, dec, parallax, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag
+FROM
+    gaiadr3.gaia_source
+WHERE
+    parallax IS NOT NULL
+    AND parallax_error IS NOT NULL
+    AND ruwe IS NOT NULL
+    AND phot_g_mean_mag IS NOT NULL
+    AND phot_g_mean_mag IS NOT NULL
+    AND phot_g_mean_flux_over_error IS NOT NULL
+    AND phot_bp_mean_mag IS NOT NULL
+    AND phot_rp_mean_mag IS NOT NULL
+    AND parallax > 0
+"""
+
+def fetch_data():
+    # TODO: Add error handling
+    job = Gaia.launch_job(adql_query) # Change to async later
+    results = job.get_results()
+    return results
